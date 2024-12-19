bec894ae4d3ebc8d677036a9729e39a4cf76ae46
Oli Loades <oli.loades@gmail.com>
2024-12-19 17:58:16 +0000

    Updated express json middleware limit to 1mb

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { requestLogger } from './common/middleware/request-logger.middleware';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '1mb' }));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.use(requestLogger);
   await app.listen(process.env.PORT ?? 3000);
