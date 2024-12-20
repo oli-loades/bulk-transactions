bc4436b465736d597ab245c8a6019b0b1188d740
Oli Loades <oli.loades@gmail.com>
2024-12-20 00:45:43 +0000

    Removed magic strings

---
+++ src/contants.ts
@@ -0,0 +1,5 @@
+export const STATUS = {
+  PENDING: 'PENDING',
+  SUCCESS: 'SUCCESS',
+  FAILED: 'FAILED',
+};
--- src/modules/transactions/transactions.service.ts
+++ src/modules/transactions/transactions.service.ts
@@ -1,18 +1,18 @@
 import { Model } from 'mongoose';
-import { Injectable, Inject, Logger } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Transaction, TransactionDocument } from './schemas/transaction.schema';
 import { BulkCreateTransactionDto } from './dto/bulk-create-transaction.dto';
 import { Types } from 'mongoose';
 import { AccountsService } from '../accounts/accounts.service';
-
+import { STATUS } from '../../contants';
 @Injectable()
 export class TransactionsService {
   private readonly logger = new Logger(TransactionsService.name);
 
   constructor(
     @InjectModel(Transaction.name) private transactionModel: Model<Transaction>,
-    @Inject(AccountsService) private readonly accountsService: AccountsService,
+    private readonly accountsService: AccountsService,
   ) {}
 
   async bulkCreate(
@@ -29,14 +29,14 @@ export class TransactionsService {
     id: Types.ObjectId,
     success: boolean,
   ): Promise<TransactionDocument> {
-    const status = success ? 'SUCCESS' : 'FAILED';
+    const status = success ? STATUS.SUCCESS : STATUS.FAILED;
     return this.transactionModel.findByIdAndUpdate(id, { status }).exec();
   }
 
   async processTransaction(transactionId: Types.ObjectId): Promise<boolean> {
     let success = false;
     const transaction = await this.getById(transactionId);
-    if (transaction && transaction.status === 'PENDING') {
+    if (transaction && transaction.status === STATUS.PENDING) {
       // issue - transaction.account has the type of Types.ObjectId but when passed to accountsService.getById acts as string
       const accountId = new Types.ObjectId(transaction.account);
       const account = await this.accountsService.getById(accountId);
